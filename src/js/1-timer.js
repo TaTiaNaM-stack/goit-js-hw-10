bfaaf070fe5d8a3453de0e9f466ed0ec1010915b
TaTiaNaM <maluchenko.tania@gmail.com>
2026-01-09 22:55:31 +0200

    css

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,16 @@
 console.log('Timer module loaded');
+
+const flatpickr = require("flatpickr");
+
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
