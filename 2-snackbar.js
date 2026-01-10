a68e9443e070bbc44d6b1da857a4358bbd60b898
TaTiaNaM-stack <maluchenko.tania@gmail.com>
2026-01-10 19:54:47 +0000

    Deploying to gh-pages from @ TaTiaNaM-stack/goit-js-hw-10@bd5cef6f1a37806b9d4148bbf117b051aebe7a06 🚀

--- 2-snackbar.js
+++ 2-snackbar.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Snackbar module loaded");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-CZ4EWmTT.js";console.log("Snackbar module loaded");
 //# sourceMappingURL=2-snackbar.js.map
