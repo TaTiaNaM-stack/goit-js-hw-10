b0a918bd8344bfdd7aaa91a46e8b4b2ab315ccb4
TaTiaNaM <maluchenko.tania@gmail.com>
2026-01-10 16:46:10 +0200

    alert

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,6 +1,6 @@
 console.log('Timer module loaded');
 
-const flatpickr = require("flatpickr");
+import flatpickr from "flatpickr";
 
 import "flatpickr/dist/flatpickr.min.css";
 
@@ -14,3 +14,23 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+const NOTIFICATION_DELAY = 3000;
+let timeoutId = null;
+const notification = document.querySelector('.js-alert');
+
+showNotification();
+
+function showNotificationClick() {}
+
+function showNotification() {
+  notification.classList.add('visible');
+
+  setTimeout(() => {
+    notification.classList.remove('visible');
+}, NOTIFICATION_DELAY);
+
+console.log('Timer module initialized');
+}
+
+function hideNotification() {}
