bf7b6b031efed3a5db4c95af63ec4e4d634125ee
TaTiaNaM-stack <maluchenko.tania@gmail.com>
2026-01-10 14:46:41 +0000

    Deploying to gh-pages from @ TaTiaNaM-stack/goit-js-hw-10@b0a918bd8344bfdd7aaa91a46e8b4b2ab315ccb4 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      *//* empty css                      */console.log("Timer module loaded");require("flatpickr");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import"./assets/vendor-4re2sZo0.js";console.log("Timer module loaded");const i=3e3,o=document.querySelector(".js-alert");e();function e(){o.classList.add("visible"),setTimeout(()=>{o.classList.remove("visible")},i),console.log("Timer module initialized")}
 //# sourceMappingURL=1-timer.js.map
